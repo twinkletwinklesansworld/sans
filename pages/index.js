4fedeeaca2e5a773efde35574a6fe18e826fadc0
소루 || soru_love <64895958+sorulove@users.noreply.github.com>
2020-08-26 20:33:32 +0900

    Card content added

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,13 @@ export default function Home(props) {
                         </CardContent>
                     </Card>
                 </Grid>
+             <Grid item xs={12} md={6}>
+                    <Card>
+                        <CardContent>
+                            공지사항
+                        </CardContent>
+                    </Card>
+                </Grid>
             </Grid>
         </Layout>
     )

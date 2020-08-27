6c6180b0fb0f64a28570c44be6bfa1fb859d724e
piese1028 <61900596+piese1028@users.noreply.github.com>
2020-08-27 20:59:27 +0900

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -22,14 +22,14 @@ class Login extends Component {
                     <Grid item xs={12} md={6}>
                         <Card>
                             <CardContent>
-                                <Typography variant="h6" align="center">로그인</Typography>
+                                <Typography variant="h6" align="center">LOGIN</Typography>
                                 <Grid item>
-                                    <TextField color="secondary" value={this.state.id} required onChange={e => this.setState({id: e.target.value})} label="아이디 또는 이메일을 입력해 주세욤!" type="email" fullWidth style={{marginBottom: 10}}/>
-                                    <TextField color="secondary" value={this.state.password} required onChange={e => this.setState({password: e.target.value})} label="비밀번호를 입력해 주세욤!" type="password" fullWidth style={{marginBottom: 10}}/>
+                                    <TextField color="secondary" value={this.state.id} required onChange={e => this.setState({id: e.target.value})} label="[반반샌월] 아이디 또는 이메일을 입력해주세요" type="email" fullWidth style={{marginBottom: 10}}/>
+                                    <TextField color="secondary" value={this.state.password} required onChange={e => this.setState({password: e.target.value})} label="[반반샌월] 비밀번호를 입력해주세요" type="password" fullWidth style={{marginBottom: 10}}/>
                                     <Grid container spacing={2}>
                                         <Grid item xs={12} md={6}>
                                             <Button variant="contained" color="primary" onClick={async () => {
-                                                this.props.enqueueSnackbar('처리중이에욤! 잠시만 기다려주세욤!', {
+                                                this.props.enqueueSnackbar('[반반샌월] 처리 중입니다', {
                                                     variant: 'info'
                                                 })
                                                 this.setState({processing: true})
@@ -52,7 +52,7 @@ class Login extends Component {
                                                     })
                                                 }
                                                 if (data.token) {
-                                                    this.props.enqueueSnackbar('로그인 완료~! 샌즈 월드를 즐겁게 이용해주세요!', {
+                                                    this.props.enqueueSnackbar('[반반샌월] 로그인이 완료되었습니다', {
                                                         variant: 'success'
                                                     })
                                                     localStorage.setItem('token', data.token)
